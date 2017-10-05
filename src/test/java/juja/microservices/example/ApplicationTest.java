75d661cf08073dcd098737aa7d1f81caaa1f4c9c
kuznetsov.danil <kuznetsov.danil.v@gmail.com>
2017-10-05 15:48:39 +0300

    add base project template

---
+++ src/test/java/juja/microservices/example/ApplicationTest.java
@@ -0,0 +1,15 @@
+package juja.microservices.example;
+
+import org.junit.Test;
+
+/**
+ * @author Danil Kuznetsov (kuznetsov.danil.v@gmail.com)
+ */
+
+public class ApplicationTest {
+
+    @Test
+    public void test(){
+
+    }
+}
